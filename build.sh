36dd78efee89c28e4fbc5ea64d49e03b745c3a0c
neil <neilgit@neilpang.com>
2023-12-06 20:43:54 +0100

    minor

--- build.sh
+++ build.sh
@@ -204,6 +204,12 @@ sleep 30
 
 ###############################################################
 
+if $vmsh isRunning $osname; then
+  if !$vmsh shutdownVM $osname; then
+    echo "shutdown error"
+  fi
+fi
+
 while $vmsh isRunning $osname; do
   sleep 5
 done
